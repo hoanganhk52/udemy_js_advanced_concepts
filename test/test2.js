a76aaa8b0c96c02bf8480387c500e987229e1b17
hoanganhk52 <hoanganhctm7@gmail.com>
2019-03-26 06:24:10 +0700

    67. Higher order function

--- test/test2.js
+++ test/test2.js
@@ -5,3 +5,10 @@ function getMaxNumber(arr){
 }
  
 console.log(getMaxNumber(array)); // should return 3
+
+
+var user1 = {name : "nerd", org: "dev"};
+var user2 = {org: "dev", name : "nerd"};
+
+var eq = JSON.stringify(user1) === JSON.stringify(user2);
+console.log(eq);
